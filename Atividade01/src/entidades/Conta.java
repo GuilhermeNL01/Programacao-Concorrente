60bc59b3a9dce654b0b0f4bb0259093ab8f0b9e9
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-04-20 16:23:47 -0300

    Lógica da Entidade Conta

--- Atividade01/src/entidades/Conta.java
+++ Atividade01/src/entidades/Conta.java
@@ -1,4 +1,27 @@
 package entidades;
 
 public class Conta {
+    private String titular;
+    private double saldo;
+
+    public Conta(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public synchronized void creditar(double valor) {
+        saldo += valor;
+    }
+
+    public synchronized void debitar(double valor) {
+        saldo -= valor;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
