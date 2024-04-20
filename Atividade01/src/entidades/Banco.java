08e96dba3dd1ccc6e373d1ab9a024fb7e1b4fd8b
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-04-20 16:18:29 -0300

    Lógica da Entidade Banco

--- Atividade01/src/entidades/Banco.java
+++ Atividade01/src/entidades/Banco.java
@@ -1,4 +1,14 @@
 package entidades;
 
 public class Banco {
+    public synchronized void transferencia(Conta origem, Conta destino, double valor) {
+        if (origem.getSaldo() >= valor) {
+            origem.debitar(valor);
+            destino.creditar(valor);
+            System.out.println("Transferência de R$" + valor + " realizada de " + origem.getTitular() +
+                    " para " + destino.getTitular());
+        } else {
+            System.out.println("Saldo insuficiente para transferência de " + origem.getTitular());
+        }
+    }
 }
