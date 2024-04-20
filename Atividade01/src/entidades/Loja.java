9cd36d7588b8c014917c1f11288c680241ddd6e7
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-04-20 16:21:12 -0300

    Lógica da Entidade Loja

--- Atividade01/src/entidades/Loja.java
+++ Atividade01/src/entidades/Loja.java
@@ -1,4 +1,20 @@
 package entidades;
 
 public class Loja {
+    private Conta conta;
+
+    public Loja(Conta conta) {
+        this.conta = conta;
+    }
+
+    public synchronized void pagarFuncionario(Funcionario funcionario) {
+        double salario = funcionario.getSalario();
+        if (conta.getSaldo() >= salario) {
+            conta.debitar(salario);
+            funcionario.receberSalario(salario);
+            System.out.println("Pagamento de R$" + salario + " para " + funcionario.getNome() + " realizado");
+        } else {
+            System.out.println("Saldo insuficiente para pagar " + funcionario.getNome());
+        }
+    }
 }
